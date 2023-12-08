c0dbfafba3edda8e024c348bac972edfb21bd63d
shantihedelin <shanti.hedelin@chasacademy.se>
2023-12-08 14:26:33 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        caveat: ["Caveat"],
+        sans: ["'Roboto Condensed'", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-  // Add the following line to enable Tailwind CSS
-  // Make sure it's the last item in your configuration
-  // and before any other CSS or PostCSS plugins
-  corePlugins: {
-    preflight: false, // If you're not using the Preflight plugin
-  },
 };
